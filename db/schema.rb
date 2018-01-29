25606bf160f461b971ea66423899dab4a508a1a2
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-01-29 16:38:05 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180129162936) do
+ActiveRecord::Schema.define(version: 20180129163756) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
